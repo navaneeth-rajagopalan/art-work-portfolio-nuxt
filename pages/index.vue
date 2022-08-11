cf1edd07e749967c405c049b51aa764b54284b62
Navaneeth Rajagopalan <navaneeth.rajagopalan.05@gmail.com>
2022-08-11 20:16:57 +1000

    Add nuxt files
    Signed-off-by: Navaneeth Rajagopalan <navaneeth.rajagopalan.05@gmail.com>

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  head() {
+    return {
+      title: "Pradisa's Art Portfolio",
+      meta: [
+        {
+          hid: '',
+          name: '',
+          content: '',
+        },
+      ],
+    }
+  },
+})
+</script>
